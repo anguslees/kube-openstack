bfb6ce45f31e761537e212bee9ce980a26f09aca
Angus Lees <gus@inodes.org>
2015-01-02 16:55:03 +1100

    Fix neutron _wrap.sh conf expansion
    Neutron's _wrap.sh explicitly listed the files to replace, and broke
    with the l3_agent.ini split/rename.  Replace with a more robust *.in
    glob, and move ml2_conf.ini into the same directory to remove that
    unnecessary complication.

--- docker/neutron/_wrap.sh
+++ docker/neutron/_wrap.sh
@@ -9,8 +9,8 @@ PATH=$PATH:/usr/local/bin
 : ${HOSTNAME:=$(hostname)}
 export MY_IP HOSTNAME
 
-for f in neutron.conf l3_agent.ini metadata_agent.ini plugins/ml2/ml2_conf.ini; do
-    perl -ple 's/\$ENV\[(\w+)\]/$ENV{$1}/eg' </etc/neutron/$f.in >/etc/neutron/$f
+for f in /etc/neutron/*.in; do
+    perl -ple 's/\$ENV\[(\w+)\]/$ENV{$1}/eg' <$f >${f%.in}
 done
 
 #exec su --preserve-environment -s $realbin user -- "$@"
