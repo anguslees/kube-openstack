a4efc745370dd2a93701119440cd46894cce9975
Angus Lees <gus@inodes.org>
2015-03-17 16:54:02 +1100

    Rewrite tempest container to use venv

--- docker/tempest/_wrap.sh
+++ docker/tempest/_wrap.sh
@@ -1,17 +1,28 @@
 #!/bin/sh
 
-set -e -x
+set -e
 
-realbin=/usr/local/bin/tempest_user.sh
-PATH=$PATH:/usr/local/bin
+. /home/install/venv/bin/activate
 
 : ${MY_IP:=$(hostname -i)}
 : ${HOSTNAME:=$(hostname)}
 export MY_IP HOSTNAME
 
-for f in /etc/tempest/tempest.conf; do
-    perl -ple 's/\$ENV\[(\w+)\]/$ENV{$1}/eg' <$f.in >$f
-done
+# TODO(gus): add support for env vars in openstack config files,
+# instead of this code.
+find $ETC_IN -type f -print |
+    while read orig; do
+	out=$ETC/${orig#$ETC_IN/}
+	mkdir -p ${out%/*}
+	case "$orig" in
+	    *.in)
+		out=${out%.in}
+		perl -ple 's/\$ENV\[(\w+)\]/$ENV{$1}/eg' <$orig >$out
+		;;
+	    *)
+		ln -sf $orig $out
+		;;
+	esac
+    done
 
-#exec $realbin "$@"
-exec su --preserve-environment -s $realbin user -- "$@"
+exec "$@"
--- docker/tempest/tempest_user.sh
+++ docker/tempest/tempest_user.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+# TODO(gus): If _wrap.sh sourced files in a directory somewhere, then
+# this additional wrapper would be unnecessary.
 . /home/user/openrc
 
-exec /usr/local/bin/run-tempest-stress "$@"
+exec /usr/local/bin/_wrap.sh run-tempest-stress "$@"
